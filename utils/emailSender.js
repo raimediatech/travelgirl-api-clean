9456f6d098a41a395488d639e3189b2f5270ea1e
Ajay Rai <raimediatech@gmail.com>
2025-05-12 02:24:45 +0530

    Fix syntax error in emailSender.js (double comma)

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -4,26 +4,28 @@ import pug from "pug";
 import constants from "./constants.js";
 
 const sendEmail = async (name, email, subject, message) => {
- 
+  const isSecure = Number(constants.CONST_SMTP_PORT) === 465;
+
   const transporter = nodemailer.createTransport({
     host: constants.CONST_SMTP_HOST,
     port: constants.CONST_SMTP_PORT,
-    secure: Number(constants.CONST_SMTP_PORT) === 465,
-    requireTLS: Number(constants.CONST_SMTP_PORT) !== 465,
-    tls: { rejectUnauthorized: false },,
+    secure: isSecure,          // SSL/TLS for port 465
+    requireTLS: !isSecure,     // STARTTLS for 587/25
+    tls: { rejectUnauthorized: false },
     auth: {
       user: constants.CONST_SMTP_USER,
       pass: constants.CONST_SMTP_PASSWORD,
     },
-   
   });
+
   transporter.verify(function (error, success) {
     if (error) {
       console.log(error);
     } else {
-      console.log("Server is ready to take our messages");
+      console.log("SMTP server is ready to send messages");
     }
   });
+
   const info = await transporter.sendMail({
     from: `${constants.CONST_APP_NAME} <${constants.CONST_SMTP_FROM_ADDRESS}>`,
     to: email,
@@ -34,100 +36,99 @@ const sendEmail = async (name, email, subject, message) => {
 };
 
 const sendRegistrationOtp = async (userData, otpData) => {
-  let templateDir = "templates/";
-  let messageBody = pug.renderFile(`${templateDir}registrationEmailOtp.pug`, {
+  const templateDir = "templates/";
+  const messageBody = pug.renderFile(`${templateDir}registrationEmailOtp.pug`, {
     name: userData.firstName,
     email: userData.email,
     otp: otpData.randomOtp,
     logo: constants.CONST_APP_LOGO,
   });
-  let subject = i18n.__("lang_registration_otp");
+  const subject = i18n.__("lang_registration_otp");
   await sendEmail(userData.name, userData.email, subject, messageBody);
   return true;
 };
 
 const resendOtp = async (userData, otpData) => {
-  let templateDir = "templates/";
-  let messageBody = pug.renderFile(`${templateDir}resendEmailOtp.pug`, {
+  const templateDir = "templates/";
+  const messageBody = pug.renderFile(`${templateDir}resendEmailOtp.pug`, {
     name: userData.name,
     email: userData.email,
     otp: otpData.randomOtp,
     logo: constants.CONST_APP_LOGO,
   });
-  let subject = i18n.__("lang_resend_otp");
+  const subject = i18n.__("lang_resend_otp");
   await sendEmail(userData.name, userData.email, subject, messageBody);
   return true;
 };
 
 const forgotPasswordOtp = async (userData, otpData) => {
-  let templateDir = "templates/";
-  let messageBody = pug.renderFile(`${templateDir}forgotPasswordOtp.pug`, {
+  const templateDir = "templates/";
+  const messageBody = pug.renderFile(`${templateDir}forgotPasswordOtp.pug`, {
     name: userData.name,
     email: userData.email,
     otp: otpData.randomOtp,
     logo: constants.CONST_APP_LOGO,
   });
-  let subject = i18n.__("lang_forgot_password_otp");
+  const subject = i18n.__("lang_forgot_password_otp");
   await sendEmail(userData.name, userData.email, subject, messageBody);
   return true;
 };
 
 const sendAdminEmail = async (email) => {
-  let adminEmail = "capriadmin@yopmail.com";
-  let templateDir = "templates/";
-  let messageBody = pug.renderFile(`${templateDir}emailRequest.pug`, {
+  const adminEmail = "capriadmin@yopmail.com";
+  const templateDir = "templates/";
+  const messageBody = pug.renderFile(`${templateDir}emailRequest.pug`, {
     email: email,
     logo: constants.CONST_APP_LOGO,
   });
-  let subject = i18n.__("lang_email_request");
-  let test=await sendEmail("", adminEmail, subject, messageBody);
-  
+  const subject = i18n.__("lang_email_request");
+  await sendEmail("", adminEmail, subject, messageBody);
   return true;
 };
 
 const sendUserEmail = async (email) => {
-  let templateDir = "templates/";
-  let messageBody = pug.renderFile(`${templateDir}userEmail.pug`, {
+  const templateDir = "templates/";
+  const messageBody = pug.renderFile(`${templateDir}userEmail.pug`, {
     email: email,
     logo: constants.CONST_APP_LOGO,
   });
-  let subject = i18n.__("lang_Exclusive_Pre-Sign_Up_Confirmation!");
+  const subject = i18n.__("lang_Exclusive_Pre-Sign_Up_Confirmation!");
   await sendEmail("", email, subject, messageBody);
   return true;
 };
 
 const sendRegistrationApprovalMail = async (userData) => {
-  let templateDir = "templates/";
-  let messageBody = pug.renderFile(`${templateDir}userApprovalMail.pug`, {
+  const templateDir = "templates/";
+  const messageBody = pug.renderFile(`${templateDir}userApprovalMail.pug`, {
     name: userData.firstName,
-    email:userData.email,
-    logo: constants.CONST_APP_LOGO, 
+    email: userData.email,
+    logo: constants.CONST_APP_LOGO,
   });
-  let subject = i18n.__("lang_account_approval_email");
+  const subject = i18n.__("lang_account_approval_email");
   await sendEmail("", "capriadmin@yopmail.com", subject, messageBody);
   return true;
 };
 
-const contactSupport = async (email,name) => {
-  let templateDir = "templates/";
-  let messageBody = pug.renderFile(`${templateDir}contactSupport.pug`, {
+const contactSupport = async (email, name) => {
+  const templateDir = "templates/";
+  const messageBody = pug.renderFile(`${templateDir}contactSupport.pug`, {
     name: name,
-    email:email,
-    logo: constants.CONST_APP_LOGO, 
+    email: email,
+    logo: constants.CONST_APP_LOGO,
   });
-  let subject = i18n.__("lang_email");
+  const subject = i18n.__("lang_email");
   await sendEmail("", "capriadmin@yopmail.com", subject, messageBody);
   return true;
 };
 
 const emailSender = {
-  sendRegistrationOtp: sendRegistrationOtp,
-  resendOtp: resendOtp,
-  forgotPasswordOtp: forgotPasswordOtp,
-  sendUserEmail:sendUserEmail,
-  sendAdminEmail:sendAdminEmail,
-  sendRegistrationApprovalMail:sendRegistrationApprovalMail,
-  contactSupport:contactSupport
+  sendRegistrationOtp,
+  resendOtp,
+  forgotPasswordOtp,
+  sendUserEmail,
+  sendAdminEmail,
+  sendRegistrationApprovalMail,
+  contactSupport,
 };
 
 export default emailSender;
